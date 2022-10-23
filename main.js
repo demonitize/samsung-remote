4e80f9673e0d1cb90392b12e03de300c4b3b95ea
unknown <MediaProjection@clcmerced.local>
2022-10-23 11:33:34 -0700

    yes

---
+++ main.js
@@ -0,0 +1,24 @@
+const CTL = require("samsung-remote");
+const express = require("express");
+
+
+const remote = new CTL({
+    ip: '192.168.1.13' // required: IP address of your Samsung Smart TV
+});
+ 
+remote.send('KEY_VOLUP', (err) => {
+    if (err) {
+        throw new Error(err);
+    } else {
+        // command has been successfully transmitted to your tv
+    }
+});
+ 
+// check if TV is alive (ping)
+remote.isAlive((err) => {
+    if (err) {
+        throw new Error('TV is offline');
+    } else {
+        console.log('TV is ALIVE!');
+    }
+});
